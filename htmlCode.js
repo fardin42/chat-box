08d863285ccff118f7e921e684a3c084fc8f60b8
fardin42 <fssaiyyad42@gmail.com>
2024-05-06 11:35:06 +0530

    first commit

---
+++ htmlCode.js
@@ -0,0 +1,52 @@
+$(".body").html(`  <div class="container">
+
+<div id="box" class="d-none">
+    <div class="ct-bx shadow" id="chatBox">
+        <div>
+            <div class="head">
+                <h1 style="margin-left: 1rem;">KIRA
+                    <span id="netIndi"></span>
+                </h1>
+                <ul>
+                    <li id="netIndi"></li>
+                </ul>
+               <a href="comfirm.html"> <button type="button" class="button-small">end chat</button></a>
+            </div>
+            <div id="chatBase">
+
+            </div>
+            <form action="">
+
+                <textarea class="text-element " name="" id="logMe" cols="25" rows="1"
+                    placeholder="enter text here..."></textarea>
+                <button id="send" type="button" class="button-small"><i
+                        class="fa-solid fa-paper-plane"></i></button>
+            </form>
+        </div>
+    </div>
+</div>
+<div class="btnBox">
+    <button id="chatBtn" class="button-flat"><i id="ct-icon" class="fa-regular fa-message"></i></button>
+`)
+
+$("#confirm").html(`<div class="container ">
+
+
+<div class="ct-bx norole">
+    <div style="position: relative; height:543px">
+
+        <div class="half">
+            <h1>KIRA</h1>
+        </div>
+        <div class="pop">
+            <h5>Do you want to close this session?</h5>
+            <div class="white-box">
+               <a type="button" href="index.html"> <button id="onIt" class="button-small" type="button">cancel</button></a>
+               <a href="index.html"> <button class="button-small" style="background-color: yellow; color: black;"
+                type="button">comfirm</button></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>`)
---
+++ index.js
@@ -0,0 +1,99 @@
+
+$("#chatBtn").on("click",function(){
+    a= true
+    if (a === true) {
+      $("#chatBtn").toggleClass("button-round")
+      
+      $("#chatBtn").toggleClass("button-flat")
+      $("#ct-icon").toggleClass("fa-x")
+    $("#ct-icon").toggleClass("fa-message")
+    $("#box").toggleClass("d-block");
+        a = false
+    }else{
+        $("#box").toggleClass("d-none");
+    }
+  });
+
+
+
+
+//   this code is for textarea
+  $('textarea').each(function () {
+    
+  }).on('input', function () {
+    this.style.height = 'auto';
+   
+    this.style.height = (this.scrollHeight - 30) + 'px'; 
+  });
+
+  
+  let logMe = document.getElementById("logMe")
+  let chatBase = document.getElementById("chatBase")
+let send = document.getElementById("send")
+let net = window.navigator.onLine
+let indicator = document.getElementById("netIndi")
+
+send.addEventListener("click", function(){
+ 
+    let newP = document.createElement("p")
+    let newP2 = document.createElement("p")
+    let myDate = document.getElementById("myDate")
+    
+    newP.setAttribute("class", "yellow")
+    newP2.setAttribute("class", "crimson ")
+    myDate = new Date()
+   let  day = myDate.getHours()
+    let  mins = myDate.getMinutes()
+    
+   
+    if (net === true) {
+      
+      newP.innerHTML = `<span>${ logMe.value} <div> <h6 id="myDate"> ${day }:${mins} </h6> <i class="fa-solid fa-check"></i> </div></span>`;
+      
+       if (logMe.value === "hi") {
+        function timeDelay() {
+          newP2.innerHTML = `<span >Hello<div> <h6 id="myDate"> ${day }:${mins} </h6> <i class="fa-solid fa-check"></i> </div></span>`;
+        }
+       
+        
+      }else if (logMe.value === "how are you") {
+         function timeDelay() {
+           newP2.innerHTML = `<span>i am fine. how about you?<div> <h6 id="myDate"> ${day }:${mins} </h6> <i class="fa-solid fa-check"></i> </div></span>`;
+        }
+       
+      }else if (logMe.value === "fine") {
+        function timeDelay() {
+          newP2.innerHTML = `<span>thats good to hear.<div> <h6 id="myDate"> ${day }:${mins} </h6> <i class="fa-solid fa-check"></i> </div></span>`;
+        }
+        
+      }else{
+        function timeDelay() {
+          newP2.innerHTML = `<span class="anim-typewriter">What?<div> <h6 id="myDate"> ${day }:${mins} </h6> <i class="fa-solid fa-check"></i> </div></span>`;
+        }
+        
+      }
+    } else {
+      function timeDelay() {
+          newP.innerHTML = `<span>${ logMe.value} <div> <h6 id="myDate"> ${day }:${mins} </h6> <i class="fa-solid fa-clock"></i> </div></span> `;
+      }
+      
+    }
+    setTimeout(timeDelay, 1000)
+    chatBase.appendChild(newP)
+    chatBase.appendChild(newP2)
+  
+})
+
+
+function indicatorNet() {
+  if (net === true) {
+    indicator.innerHTML = `<b class="green">&bull;</b> Online`
+  } else {
+    indicator.innerHTML = `<b class="red">&bull;</b> Offline`
+  }
+}
+indicatorNet()
+
+
+
+  
