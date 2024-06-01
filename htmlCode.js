fda861112b720b8b0867798679f611852c5ebd62
skmoin5 <johndoe@example.com>
2024-06-01 17:33:52 +0530

    another change

--- htmlCode.js
+++ htmlCode.js
@@ -1,38 +1,38 @@
-$(".body").html(`  <div class="container">
+// $(".body").html(`  <div class="container">
 
-<div id="box" class="d-none">
-    <div class="ct-bx shadow" id="chatBox">
+// <div id="box" class="d-none">
+//     <div class="ct-bx shadow" id="chatBox">
     
-        <div>
-            <div class="head">
-                <h1 style="margin-left: 1rem;">KIRA
-                    <span id="netIndi"></span>
-                </h1>
-                <ul>
-                    <li id="netIndi"></li>
-                </ul>
-               <a href="comfirm.html"> <button type="button" class="button-small">end chat</button></a>
-            </div>
-            <div id="chatBase">
+//         <div>
+//             <div class="head">
+//                 <h1 style="margin-left: 1rem;">KIRA
+//                     <span id="netIndi"></span>
+//                 </h1>
+//                 <ul>
+//                     <li id="netIndi"></li>
+//                 </ul>
+//                <a href="comfirm.html"> <button type="button" class="button-small">end chat</button></a>
+//             </div>
+//             <div id="chatBase">
 
-            </div>
-            <form action="">
+//             </div>
+//             <form action="">
 
-                <textarea class="text-element " name="" id="logMe" cols="25" rows="1"
-                    placeholder="enter text here..."></textarea>
-                <button id="send" type="button" class="button-small"><i
-                        class="fa-solid fa-paper-plane"></i></button>
+//                 <textarea class="text-element " name="" id="logMe" cols="25" rows="1"
+//                     placeholder="enter text here..."></textarea>
+//                 <button id="send" type="button" class="button-small"><i
+//                         class="fa-solid fa-paper-plane"></i></button>
 
-            </form>
+//             </form>
            
-        </div>
-    </div>
-</div>
-<div class="btnBox">
-    <button id="chatBtn" class="button-flat"><i id="ct-icon" class="fa-regular fa-message"></i></button>
-    </div>
-    <div id="music"></div>
-`)
+//         </div>
+//     </div>
+// </div>
+// <div class="btnBox">
+//     <button id="chatBtn" class="button-flat"><i id="ct-icon" class="fa-regular fa-message"></i></button>
+//     </div>
+//     <div id="music"></div>
+// `)
 
 $("#confirm").html(`<div class="container ">
 
